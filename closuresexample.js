79d732e0442991bad4cdbdaab6348647e02930ba
Marco L <ibepolo@gmail.com>
2015-04-09 14:54:49 -0400

    update

--- closuresexample.js
+++ closuresexample.js
@@ -1,19 +1,19 @@
-var b = function (greeting) {
+var greet = function (greeting) {
 
     return function (name) {
         alert(greeting + " " + name + ". It is nice to meet you.");
     };
 };
 
-var getB = b("Hello");
-getB("Paul"); // Hello Paul. It is nice to meet you.
-getB("Mike"); // Hello Mike. It is nice to meet you.
-getB("Jim"); //Hello Jim. It is nice to meet you.
+var getGreet = greet("Hello");
+getGreet("Paul"); // Hello Paul. It is nice to meet you.
+getGreet("Mike"); // Hello Mike. It is nice to meet you.
+getGreet("Jim"); //Hello Jim. It is nice to meet you.
 
 
-var getBHi = b("Hi");
-var getBHola = b("Hola");
-var getBKonichiwa = b("Konichiwa");
+var getBHi = greet("Hi");
+var getBHola = greet("Hola");
+var getBKonichiwa = greet("Konichiwa");
 
 getBHi("Andres"); // Hi Andres. It is nice to meet you.
 getBHola("Kentaro"); // Hola Kentaro. It is nice to meet you.  
