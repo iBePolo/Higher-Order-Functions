3053ecec4a5a98be17e538dd27febe6cb89fec34
Marco L <ibepolo@gmail.com>
2015-04-16 15:18:24 -0400

    map method & multiply function

--- map method multiply function.js
+++ map method multiply function.js
@@ -2,6 +2,7 @@
 Using the map method on an array. New tho this method but I can already start to see how useful and time saving it must be.
 */
 
+------ The Code ------
 
 var numArr = [1, 2, 3];
 
@@ -12,5 +13,16 @@ var mapMethodFunc = numArr.map(function (arr1) {
 });
 
 
+
+------------------------------
+------ Running the code ------
+------------------------------
+
 var myNewFunc = mapMethodFunc;
+// Returns: undefined
 console.log(myNewFunc);
+// Returns: [3, 6, 9] & undefined
+
+
+myNewFunc;
+// Returns: [3, 6, 9]
