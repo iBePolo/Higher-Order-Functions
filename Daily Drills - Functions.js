79c3ba00d1cfff35b690451f8dad22a59aae3958
Marco L <ibepolo@gmail.com>
2015-04-17 17:24:30 -0400

    Practicing Higher-Order Functions

--- Daily Drills - Functions.js
+++ Daily Drills - Functions.js
@@ -1,3 +1,7 @@
+// This file is dedicated to practicing Higher-Order Functions.
+
+************************************************************************************
+
 /*
 The multX3 function gets passed into the numXNum function. Since multX3 needs a
 value for the x parameter, it pulls it from the return of numXNum.
@@ -17,10 +21,39 @@ var a = numXNum(2, multX3);
 alert(a); // Returns ---> 12
 
 ************************************************************************************
+// Apply an action to a number
+
+function x2(x, action) {
+	action(x);
+} // <--- Does not need a semicolon because it's a declared function.
+
+
+x2(3, function(x){return x * 2};); // ---> undefined
+
+x2(3, function(x){console.log(x * 2);}); // ---> 6 & undefined
+
+var a = 5;
+x2(a, function(x){console.log(x * 2);}); // ---> 10 & undefined
 
 ************************************************************************************
+var f = function (a, b, c) {
+	return c(a + b);
+};
 
+
+f(5, 6, function (x) {return x * 3;}); // ---> 33
+
+var m = f(5, 6, function (x) {return x * 3;}); // ---> undefined
+m; // ---> 33
 ************************************************************************************
+var eachItem = function (myArr, action) {
+	for (var i = 0; i < myArr.length; i++)
+		action(myArr[i]);
+}; // <--- Needs a semicolon because it's a function expression.
+
+var newArr = ["Hello", "Goodbye"];
+
+eachItem(newArr, function (x) {console.log(x);}); // <--- Hello, Goodbye & undefined
 
 ************************************************************************************
 
