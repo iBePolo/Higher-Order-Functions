04cda465ae9ceed5869b542e1ca1881ba7d0aa50
Marco L <ibepolo@gmail.com>
2015-04-18 23:30:52 -0400

    Practice with closures

--- Daily Drills - closures.js
+++ Daily Drills - closures.js
@@ -21,9 +21,8 @@ h(); // <--- "Today is Fri Apr 17 2015 18:09:51 GMT-0400 (EDT). Is there anythin
 
 ---------------------- Same Code Part 2 -------------------------------------------
 
-// This time I created this function in the global scope & returning the "name" of the function w/a parameter  later. ----> "return m(userName);"
+// This time I created this function in the global scope & returning the "name" of the function w/a parameter later. ----> "m(userName);"
 var m = function (x) {
-	// The return statement is accessing "userName" whic is a global variable.
 	return "Today is " + Date() + "." + " Is there anything else I can help you with today, " + x + "?";
 };
 
@@ -46,14 +45,47 @@ var h = userFirstName(userName); // <--- undefined
 h(); // ---> Uncaught TypeError: h is not a function
 
 /*
-I think that the reason that "h;" without parens returns the value this time is because, since in the userFirstName 
+I think that the reason that "h;" without parenthesis returns the value this time is because, since in the userFirstName 
 function we are returning a value, when we assign "h" the value of userFirstName(userName) -  We are assigning the 
 value that is returned. In this case we aren't returning a function. We are returning a value. This means that h does 
-not have to be called w/parens because the value that was returned from the userFirstName function is not a function. 
+not have to be called w/parenthesis because the value that was returned from the userFirstName function is not a function. 
 So "h" does not need to be called as a function because it is not one. It is a string which means that all we have to 
 do to call it is type ---> h;
 */
 h; // ---> "Today is Sat Apr 18 2015 22:23:15 GMT-0400 (EDT). Is there anything else I can help you with today, Marco?"
+
+
+
+---------------------- Same Code Part 3 -------------------------------------------
+
+// This time I am simply returning a parameter fron the userFirstName function. Then I am passing a function with a parameter through it and assigning the value to "i".
+var m = function (x) {
+	return "Today is " + Date() + "." + " Is there anything else I can help you with today, " + x + "?";
+};
+
+
+var userName = prompt("Please enter your first name."); // <--- Entered "Marco"
+
+
+// a very simple function that returns whatever value is passed. Since everything in JS is an object, we can pass anything.
+var userFirstName = function (o) {
+	return o;
+};
+
+
+
+// Here we are passing a variable that represents a string.
+var h = userFirstName(userName); // ---> undefined
+h; // ---> "Marco"
+
+// Here we are passing a function w/a parameter.
+userFirstName(m(userName)); // ---> "Today is Sat Apr 18 2015 23:06:37 GMT-0400 (EDT). Is there anything else I can help you with today, Marco?"
+
+// This one assigns the value returned from passing the function "m" w/it's parameter through the function named "userFirstName"
+var i = userFirstName(m(userName)); // ---> undefined;
+// So "userFirsName" the "m" function which carries out a task of it's own. 
+
+i; // ---> "Today is Sat Apr 18 2015 23:03:09 GMT-0400 (EDT). Is there anything else I can help you with today, Marco?"
 
 
 
